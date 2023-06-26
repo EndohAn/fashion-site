d0e3fa93e16ff2ddedf648d94e0377849237ebca
unknown <flowerrosa76@gmail.com>
2023-06-26 15:24:21 +0900

    changed banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,33 +4,33 @@ import bannerImg from'./../../img/images/promo-img.jpg';
 const Banner = () =>{
     return(
         <section className='banner'>
-            <div className='container'>
+            <div className='banner__container'>
                 <div className='banner__wrapper'>
                     <div className="banner__img">
-						<img src={bannerImg} alt="bannern" />
+						<img src={bannerImg} alt="banner" />
 					</div>
                     <div className='banner__text'>  
                          <div className='banner__title' >  
                             <span class="highlight">
-								<span> PAYDAY  </span>
-							</span>
-                                           
-                             SALE NOW                       
+								<span> PAYDAY     </span>
+							</span> 
+                            <br></br>Sale
+                            Now                                                                         
                          </div>
                                <div className="banner__subtitle">
                                     Spend minimal $100 get 30% off voucher code for your next purchase
                                 </div>
-                          <div className='banner__desc'>
-                          1 June - 10 June 2021
-                          </div>
-                    <div className='banner__desc_sub'>
-                    *Terms & Conditions apply
-                    </div>
-                    <div className="promo__btn-wrapper">
+                                    <div className='banner__desc'>
+                                        1 June - 10 June 2021
+                                     </div>
+                                <div className='banner__desc_sub'>
+                                        *Terms & Conditions apply
+                                </div>
+                         <div >
 							<a href="#!" className="promo__btn">
 								Shop Now
 							</a>
-                    </div>
+                        </div>
 					</div>
                 </div>
             </div>          
